da96cc962b83c3aaf757ebee4ec2ff6c44a97963
bouzidima30 <bouzidima30@gmail.com>
2026-04-28 14:42:54 +0100

    Update ClubDetail.tsx

--- src/pages/ClubDetail.tsx
+++ src/pages/ClubDetail.tsx
@@ -64,7 +64,7 @@ const ClubDetail = () => {
                 <OptimizedImage
                   src={club.image_url}
                   alt={club.titre}
-                  className="w-full h-full object-cover"
+                  className="w-full h-auto object-cover"
                 />
               ) : (
                 <div className="w-full h-full flex items-center justify-center text-gray-400">
